34cca9c290af7c20abf57712d65b47c5146c8277
Orien Madgwick <orien.madgwick@gmail.com>
2012-06-13 22:20:58 +1000

    Expand documentation on generate method.

--- src/main/java/com/google/code/beanmatchers/ValueGenerator.java
+++ src/main/java/com/google/code/beanmatchers/ValueGenerator.java
@@ -8,6 +8,12 @@ package com.google.code.beanmatchers;
 public interface ValueGenerator<T> {
 
     /**
+     * Generate a new instance. The new should have random value.
+     * Specifically, two sequential calls to this method should
+     * result (with high probability, not guaranteed) in two
+     * distinct values - values that are not equal as per their
+     * {@code equals()} method.
+     *
      * @return A new instance of random value.
      */
     T generate();
