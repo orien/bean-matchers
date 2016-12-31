c08f60ec0d846f32d2b9ac76b3078b491a97cbf2
Orien Madgwick <_@orien.io>
2017-01-01 00:50:28 +1100

    Rename excludedProperties to properties

--- src/main/java/com/google/code/beanmatchers/HasToStringDescribingPropertiesMatcher.java
+++ src/main/java/com/google/code/beanmatchers/HasToStringDescribingPropertiesMatcher.java
@@ -9,20 +9,20 @@ import static java.util.Arrays.asList;
 public class HasToStringDescribingPropertiesMatcher<T> extends AbstractBeanToStringMatcher<T> {
 
 
-    private final List<String> excludedProperties;
+    private final List<String> properties;
 
-    public HasToStringDescribingPropertiesMatcher(TypeBasedValueGenerator valueGenerator, String... excludedProperties) {
+    public HasToStringDescribingPropertiesMatcher(TypeBasedValueGenerator valueGenerator, String... properties) {
         super(valueGenerator);
-        this.excludedProperties = asList(excludedProperties);
+        this.properties = asList(properties);
     }
 
     @Override
     protected boolean matchesSafely(Class beanType, Description mismatchDescription) {
-        return super.toStringDescribesProperties(beanType, excludedProperties, mismatchDescription);
+        return super.toStringDescribesProperties(beanType, properties, mismatchDescription);
     }
 
     public void describeTo(Description description) {
         description.appendText("bean with toString() describing class name and the properties ");
-        description.appendValue(excludedProperties);
+        description.appendValue(properties);
     }
 }
