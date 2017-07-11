2eb1107603f2ee1ed902aa2874df09d30758f908
florian.zipser <zipser@365farmnet.com>
2017-07-11 16:08:53 +0200

    created a UUID generator to generate UUIDs and registered the generator at the BeanMatchers class

---
+++ src/main/java/com/google/code/beanmatchers/UuidGenerator.java
@@ -0,0 +1,9 @@
+package com.google.code.beanmatchers;
+
+import java.util.UUID;
+
+class UuidGenerator implements ValueGenerator<UUID> {
+	public UUID generate() {
+		return UUID.randomUUID();
+	}
+}
---
+++ src/test/java/com/google/code/beanmatchers/UuidGeneratorTest.java
@@ -0,0 +1,41 @@
+package com.google.code.beanmatchers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.UUID;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class UuidGeneratorTest {
+
+	private UuidGenerator unitUnderTest;
+
+	@BeforeMethod
+	public void setUp() {
+		unitUnderTest = new UuidGenerator();
+	}
+
+	@Test
+	public void shouldProvideString() {
+		// when
+		UUID result = unitUnderTest.generate();
+
+		// then
+		assertThat(result, is(notNullValue()));
+	}
+
+	@Test
+	public void shouldProvideDifferingStringOnSubsequentCall() {
+		// when
+		UUID result1 = unitUnderTest.generate();
+		UUID result2 = unitUnderTest.generate();
+
+		// then
+		assertThat(result1, is(not(equalTo(result2))));
+	}
+}
