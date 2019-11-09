309756a05ae21d9b4a097f87d25eb24ba4760aaa
Orien Madgwick <_@orien.io>
2019-11-09 17:43:13 +1100

    Update hamcrest version: 1.3 -> 2.2

--- src/main/java/com/google/code/beanmatchers/BeanMatchers.java
+++ src/main/java/com/google/code/beanmatchers/BeanMatchers.java
@@ -3,7 +3,6 @@ package com.google.code.beanmatchers;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
-import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
 
 public final class BeanMatchers {
@@ -39,83 +38,67 @@ public final class BeanMatchers {
   private BeanMatchers() {
   }
 
-  @Factory
   public static Matcher<Class> hasValidGettersAndSettersFor(String... properties) {
     return new InstantiatingMatcherDecorator(isABeanWithValidGettersAndSettersFor(properties));
   }
 
-  @Factory
   public static <T> Matcher<T> isABeanWithValidGettersAndSettersFor(String... properties) {
     return new HasValidGettersAndSettersMatcher<T>(TYPE_BASED_VALUE_GENERATOR, properties);
   }
 
-  @Factory
   public static Matcher<Class> hasValidGettersAndSettersExcluding(String... properties) {
     return new InstantiatingMatcherDecorator(
         isABeanWithValidGettersAndSettersExcluding(properties));
   }
 
-  @Factory
   public static <T> Matcher<T> isABeanWithValidGettersAndSettersExcluding(String... properties) {
     return new HasValidGettersAndSettersExcludingMatcher<T>(TYPE_BASED_VALUE_GENERATOR, properties);
   }
 
-  @Factory
   public static Matcher<Class> hasValidGettersAndSetters() {
     return hasValidGettersAndSettersExcluding();
   }
 
-  @Factory
   public static <T> Matcher<T> isABeanWithValidGettersAndSetters() {
     return isABeanWithValidGettersAndSettersExcluding();
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanConstructor() {
     return new HasValidBeanConstructorMatcher();
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanHashCode() {
     return new HasValidBeanHashCodeExcludingMatcher(TYPE_BASED_VALUE_GENERATOR);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanHashCodeFor(String... properties) {
     return new HasValidBeanHashCodeForMatcher(TYPE_BASED_VALUE_GENERATOR, properties);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanHashCodeExcluding(String... properties) {
     return new HasValidBeanHashCodeExcludingMatcher(TYPE_BASED_VALUE_GENERATOR, properties);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanEquals() {
     return new HasValidBeanEqualsExcludingMatcher(TYPE_BASED_VALUE_GENERATOR);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanEqualsFor(String... properties) {
     return new HasValidBeanEqualsForMatcher(TYPE_BASED_VALUE_GENERATOR, properties);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanEqualsExcluding(String... properties) {
     return new HasValidBeanEqualsExcludingMatcher(TYPE_BASED_VALUE_GENERATOR, properties);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanToString() {
     return new HasToStringDescribingPropertiesExcludingMatcher(TYPE_BASED_VALUE_GENERATOR);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanToStringFor(String... properties) {
     return new HasToStringDescribingPropertiesMatcher(TYPE_BASED_VALUE_GENERATOR, properties);
   }
 
-  @Factory
   public static Matcher<Class> hasValidBeanToStringExcluding(String... properties) {
     return new HasToStringDescribingPropertiesExcludingMatcher(
         TYPE_BASED_VALUE_GENERATOR, properties);
